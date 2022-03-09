075c13103113b71511b60ac9d48b8c539bc3b011
WendyOsorio <98280294+Wendy-Osorio@users.noreply.github.com>
2022-03-09 09:02:43 -0600

    Subir Archivos

---
+++ MathSolutions/Data/Models/Estatus.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MathSolutions.Data.Models
+{
+    public class Estatus
+    {
+        public Estatus()
+        {
+            Cursos = new HashSet<Curso>();
+        }
+
+        public int IdEstatus { get; set; }
+        public string Estado { get; set; }
+
+        public virtual ICollection<Curso> Cursos { get; set; }
+    }
+}
